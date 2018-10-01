5a15d596fa4632f480069c2cada94146cc1e92f2
Victor <victor.outtes@gmail.com>
2018-10-01 16:50:50 -0300

    Initial commit

---
+++ clientes/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+
+def listar(request):
+    return render(request, 'aa')
+
--- project/urls.py
+++ project/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from clientes import urls as clientes_urls
 
 urlpatterns = [
+    path('clientes/', include(clientes_urls)),
     path('admin/', admin.site.urls),
 ]
